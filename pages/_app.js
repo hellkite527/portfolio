c26b467d543d6e51b15a57e91d09f2062feadcdf
Vincent Pableo <vincent.pableo@gmail.com>
2022-09-04 20:24:27 +0200

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
+import { useEffect } from "react";
+
+function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return (
+  <>
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Head>
+  <Component {...pageProps} />
+  </>
+  );
+}
+
+export default MyApp;
