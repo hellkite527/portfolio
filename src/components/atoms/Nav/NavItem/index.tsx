33680902fa7c71337d2e945729581525ca77afb1
vpableo <vincent.pableo@softvision.com>
2024-09-26 22:27:24 +0800

    NavItem overhaul

--- src/components/atoms/Nav/NavItem/index.tsx
+++ src/components/atoms/Nav/NavItem/index.tsx
@@ -1,10 +1,20 @@
-import {
-  Nav as DefaultNav,
-  NavProps as DefaultNavProps
-} from 'react-bootstrap'
+import React, { ReactNode } from 'react'
+import { Nav as DefaultNav, NavItemProps as BsNavItemProps } from 'react-bootstrap'
 
-export type NavItemProps = ComponentWithChildren & DefaultNavProps;
-
-export const NavItem = (props: NavItemProps) => {
-  return <DefaultNav.Item {...props}>{props.children}</DefaultNav.Item>
+// Define the props for the custom NavItem atom component
+export type NavItemProps = BsNavItemProps & {
+  children: ReactNode
+  eventKey?: string | number | null // Optional eventKey for navigation
+  onSelect?: (eventKey: string | number | null) => void // Optional onSelect callback
 }
+
+// Create the NavItem atom component
+const NavItem: React.FC<NavItemProps> = ({ children, eventKey, onSelect, ...rest }) => {
+  return (
+    <DefaultNav.Item eventKey={eventKey} onSelect={onSelect} {...rest}>
+      {children}
+    </DefaultNav.Item>
+  )
+}
+
+export default NavItem
